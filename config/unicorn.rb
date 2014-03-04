4b5819cd593814ddf86ca917059d9f8367993379
gyf1214 <gyf_1214@163.com>
2014-03-04 18:04:23 +0800

    listen unix socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid File.expand_path('tmp/pids/unicorn.pid', root)
 stderr_path File.expand_path('log/unicorn.log', root)
 stdout_path File.expand_path('log/unicorn.log', root)
 
-listen 3000
+listen '/tmp/unicorn.sock'
 worker_processes 2
 timeout 30
